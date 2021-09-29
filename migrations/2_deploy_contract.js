6ee7ffb42fc53d8960fb5ee2331e1e9ea0700d20
Shivam Singh <skumar@gleason.com>
2021-09-29 15:05:19 +0530

    removed private keys

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -3,6 +3,6 @@ const WhaleNetwork = artifacts.require("WhaleNetwork");
 module.exports = async function (deployer) {
   await deployer.deploy(WhaleNetwork);
   const whaleNetworkContract = await WhaleNetwork.deployed();
-  const serviceFeeCollector = "0xd279768E6F3040C04a147BC4a8D667fF067F948B";
+  const serviceFeeCollector = "SERVICE FEE COLLECTOR WALLET";
   await whaleNetworkContract.setServiceFeeCollector(serviceFeeCollector);
 };
